df90acc60613e9b3a7b973c4ea7798221d67f340
Lorenzo <lorenzogovender152@gmail.com>
2023-05-20 21:45:53 +0200

    Completed the contact form

--- simple_react_website/src/components/Contact.js
+++ simple_react_website/src/components/Contact.js
@@ -1,10 +1,58 @@
+import React, { useState } from 'react';
 
 function Contact() {
-    return (
-        <div>
-            <h1>This is the contact page</h1>
-        </div>
-    )
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send the form data to the PHP script
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('message', message);
+
+    fetch('http://127.0.0.1/php_send_mail/send.php', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => {
+        // Handle the response from the PHP script
+        console.log(response);
+        // Reset the form fields if needed
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
-export default Contact
+export default Contact;
